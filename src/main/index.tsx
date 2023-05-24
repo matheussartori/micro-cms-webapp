4dae8b75d5b1f87735ded412228cbb585167e9b3
Matheus Sartori <ms.sartori@outlook.com.br>
2023-05-24 13:11:44 -0300

    chore: add router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+
+import { Router } from '@/src/main/routes/router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 )
---
+++ src/main/routes/router.tsx
@@ -0,0 +1,12 @@
+import { Home } from '@/src/presentation/pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
