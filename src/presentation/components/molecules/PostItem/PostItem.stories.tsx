26036f80726ffbed33e2a9ee1d7379fcb54b8953
Matheus Sartori <ms.sartori@outlook.com.br>
2023-05-25 17:16:13 -0300

    fix: post item storybook

--- src/presentation/components/molecules/PostItem/PostItem.stories.tsx
+++ src/presentation/components/molecules/PostItem/PostItem.stories.tsx
@@ -1,14 +1,34 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { PostItem } from './'
 import { PostItemProps } from './PostItem'
+import { BrowserRouter } from 'react-router-dom'
 
 export default {
   title: 'Molecules/PostItem',
   component: PostItem,
+  decorators: [
+    (Story) => {
+      return (
+        <BrowserRouter>
+          {Story()}
+        </BrowserRouter>
+      )
+    }
+  ],
   args: {
     time: '25 May, 2023',
     title: 'Title',
-    shortContent: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quo expedita esse veniam illo placeat dicta, voluptatibus exercitationem possimus iste pariatur culpa iure, distinctio officia harum sapiente dolorem? Ab, sunt.'
+    shortContent: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quo expedita esse veniam illo placeat dicta, voluptatibus exercitationem possimus iste pariatur culpa iure, distinctio officia harum sapiente dolorem? Ab, sunt.',
+    post: {
+      id: 'any_id'
+    }
+  },
+  argTypes: {
+    post: {
+      control: {
+        type: null
+      }
+    }
   }
 } as Meta<PostItemProps>
 
