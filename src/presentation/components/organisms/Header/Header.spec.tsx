5a5ad1644dc7a63bcbe74cc624525ed098107557
Matheus Sartori <ms.sartori@outlook.com.br>
2023-05-26 19:20:42 -0300

    test: post context

--- src/presentation/components/organisms/Header/Header.spec.tsx
+++ src/presentation/components/organisms/Header/Header.spec.tsx
@@ -1,10 +1,16 @@
-import { describe, expect, it } from 'vitest'
+import { afterAll, describe, expect, it, vi } from 'vitest'
 import { renderWithBrowserRouter, screen } from '@/tests/test-utils'
 
 import { Header } from './'
 
 describe('Header Component', () => {
+  afterAll(() => {
+    vi.clearAllMocks()
+  })
+
   it('should render correctly', () => {
+    window.scrollTo = vi.fn() as unknown as typeof window.scrollTo
+
     renderWithBrowserRouter(
       <Header />
     )
