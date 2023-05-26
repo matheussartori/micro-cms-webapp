55b421ab1c93241f11e5ed616a0111ae04064dec
Matheus Sartori <ms.sartori@outlook.com.br>
2023-05-26 19:27:40 -0300

    feat: searchbar responsive

--- src/presentation/components/molecules/SearchBar/styles.ts
+++ src/presentation/components/molecules/SearchBar/styles.ts
@@ -2,5 +2,9 @@ import { styled } from '@/presentation/styles/stitches.config'
 
 export const Container = styled('div', {
   display: 'flex',
-  alignItems: 'center'
+  alignItems: 'center',
+
+  '@mobile': {
+    justifyContent: 'center'
+  }
 })
